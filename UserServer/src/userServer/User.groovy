2cbaa59ec94c8d037b45ec93db231c837be6b6f7
matulja <maryna.fehler@gmail.com>
2016-09-12 16:59:54 +0200

    login_start code

--- UserServer/src/userServer/User.groovy
+++ UserServer/src/userServer/User.groovy
@@ -2,19 +2,20 @@ package userServer
 
 class User {
 	
-	def name, email
-	def inetAddr = findIPAddress()
-	
-	/**
-	 * Get users IP-address
-	 */
-	String findIPAddress(){
-		try {
-			def inetAddr 
-			return inetAddr
-		}
-		catch (UnknownHostException e) {
-			println "Host not found: " + e.getMessage()
-		}
-	}
+     String name
+//, email
+//	def inetAddr = findIPAddress()
+//	
+//	/**
+//	 * Get users IP-address
+//	 */
+//	String findIPAddress(){
+//		try {
+//			def inetAddr 
+//			return inetAddr
+//		}
+//		catch (UnknownHostException e) {
+//			println "Host not found: " + e.getMessage()
+//		}
+//	}
 }
--- UserServer/src/userServer/controllers/LoginController.groovy
+++ UserServer/src/userServer/controllers/LoginController.groovy
@@ -1,5 +1,57 @@
 package userServer.controllers
 
+import groovyx.net.http.RESTClient
+
+
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.Consumes
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.Context
+
+import userServer.User
+
+@Path('/login_user')
+
+
 class LoginController {
+	
+	@Context
+	
+	
+	
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	
+	
+	def loginService (User user)
+	
+	{  
+		User myUser= new User (user."Anna")
+		eingellogteUsers.put(myUser.name, ip)  // ip
+		println "Welcome to MESSAS myUser.name"
+		
+		
+	}
+	
+	
+	static main(args)
+	{
+		
+		def br = new BufferedReader(new InputStreamReader(System.in))
+		println "Please enter your username: "
+		def myUser = br.readLine()
+		println "User name: $myUser"
+		
+		/*RESTClient client = new RESTClient('http://141.45.207.58:8080/')
+		def response = client.get(path: '/login-user')
+		assert response.status == 200
+		assert response.data == 'Login'*/
+		/*loginService()*/
+	}
+	
 
 }
