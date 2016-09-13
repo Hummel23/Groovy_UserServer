a414641e549ff3a066f2464c6a64b73fc320e238
Elif Yavuz <s0550567@htw-berlin.de>
2016-09-13 18:09:59 +0200

    fix conflict

--- UserServer/src/userServer/User.groovy
+++ UserServer/src/userServer/User.groovy
@@ -6,26 +6,14 @@ class User {
 	
 	
 	def name
-<<<<<<< HEAD
-	//def ip
-=======
 	def ip
 		
-	
-	
->>>>>>> branch 'master' of https://github.com/Hummel23/Groovy_UserServer.git
 
-<<<<<<< HEAD
-	def inetAddr = findIPAddress()
-=======
-	
-	
 	@Override
 	public String toString() {
 		return "[name=" + name + ", ip=" + ip + "]";
 	}
 	/*def inetAddr = findIPAddress()
->>>>>>> branch 'master' of https://github.com/Hummel23/Groovy_UserServer.git
 	
 	*//**
 	 * Get users IP-address
